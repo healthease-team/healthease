e4e8e2cf6be23294dffeed01c3fcf47c915a1645
Muskaan Mahabier <muskaanmahabier@gmail.com>
2026-08-04 12:54:51 -0300

    Remove unused assets

--- web1/src/routes/pharmacy/dashboard.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/pharmacy/dashboard')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/pharmacy/dashboard"!</div>
-}
